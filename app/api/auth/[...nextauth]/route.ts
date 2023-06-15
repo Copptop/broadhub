6323ce2609cb581b714ba72bcf3290cab65ae957
Copptop <91748310+Copptop@users.noreply.github.com>
2023-06-15 21:42:41 +0100

    prismaAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,28 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth"
 import type { NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import CredentialsProvider from "next-auth/providers/credentials"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import { prisma } from "@/lib/prisma"
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
+    CredentialsProvider({
+      credentials: { email: { label: "email", type: "text" }, password: { label: "Password", type: "password" } },
+      async authorize(credentials) {
+        const loginUser = await prisma.user.findUnique({ where: { email: credentials?.email } })
+
+        if (loginUser && loginUser.password === credentials?.password) {
+          return loginUser
+        } else {
+          throw new Error("Login failed, user not found or password incorrect.")
+        }
+      },
     }),
-  ]
-};
+  ],
+}
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
