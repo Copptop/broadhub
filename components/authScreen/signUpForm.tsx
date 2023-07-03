d9712ab65acfea0a72a00ad054b6d9182224568a
Copptop <91748310+Copptop@users.noreply.github.com>
2023-07-03 16:19:27 +0100

    ReWork Logo, Login Fix and Vercel analytics

--- components/authScreen/signUpForm.tsx
+++ components/authScreen/signUpForm.tsx
@@ -6,7 +6,7 @@ import { PlusIcon } from '@heroicons/react/20/solid';
 
 import { SubmitButton } from '@/components/Buttons';
 import { InputField } from '@/components/InputFields';
-import { AtSymbolIcon, FingerPrintIcon, UserIcon } from '@heroicons/react/24/outline';
+import { AtSymbolIcon, FingerPrintIcon, MinusSmallIcon, UserIcon } from '@heroicons/react/24/outline';
 
 const SignUpForm = () => {
   const [isCollapsed, setIsCollapsed] = useState(true);
@@ -35,10 +35,22 @@ const SignUpForm = () => {
             <button
               type="button"
               className="inline-flex items-center gap-x-1.5 rounded-full bg-zinc-50 dark:bg-zinc-600 px-3 py-1.5 text-sm font-semibold text-zinc-700 dark:text-zinc-300 shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-zinc-800 hover:bg-gray-50 dark:hover:bg-zinc-600"
-              onClick={(event) => { event.preventDefault(); toggleCollapse(); }}
+              onClick={(event) => {
+                event.preventDefault();
+                toggleCollapse();
+              }}
             >
-              <PlusIcon className="-ml-1 -mr-0.5 h-5 w-5 text-zinc-700 dark:text-zinc-300" aria-hidden="true" />
-              Company Code
+              {isCollapsed ? (
+                <>
+                  <PlusIcon className="-ml-1 -mr-0.5 h-5 w-5 text-zinc-700 dark:text-zinc-300" aria-hidden="true" />
+                  <span>Company Code</span>
+                </>
+              ) : (
+                <>
+                  <MinusSmallIcon className="-ml-1 -mr-0.5 h-5 w-5 text-zinc-700 dark:text-zinc-300" aria-hidden="true" />
+                  <span>Company Code</span>
+                </>
+              )}
             </button>
           </div>
           <div className={`transition-all duration-1000 ease-in-out ${isCollapsed ? 'h-0 overflow-hidden' : 'h-auto'}`}>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx,svg}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: 'class',
